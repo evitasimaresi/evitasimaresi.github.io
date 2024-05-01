6967b59817367c8ad5b26611efd4048d40dbfecb
evitaLocal <evitasima@Kloio>
2024-05-02 02:08:26 +0300

    Progressing for grid system

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -1,72 +1,107 @@
 ---
 ---
-var postCount = {{ site.posts.size }};
-
-function RandomPostsArray(postCount) {
-    var numbers = Array.from({ length: postCount }, (_, i) => i + 1);
-    for (var i = 0; i < postCount; i++) {
+function getRandomPostsArray(postCount) {
+    let numbers = Array.from({ length: postCount }, (_, i) => i + 1);
+    for (let i = 0; i < postCount; i++) {
         const j = Math.floor(Math.random() * (i + 1));
         [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
     }
-    console.log("numbers: " + numbers);
+    // console.log("numbers: " + numbers);
 }
 
-RandomPostsArray(postCount);
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+function defineImageSize(imgSurce) {
+    return new Promise((resolve, reject) => {
+        let img = new Image();
+        img.onload = function () {
+            let dimensions = { width: this.width, height: this.height };
+            // console.log("imageDimensions: ", dimensions);
+            let cellSize;
+            if (dimensions.height > dimensions.width) {
+                // console.log("vertical")
+                // Vertical image
+                cellSize = getRandomNumber(2, 4);
+            } else {
+                // console.log("horizontal")
+                // Horizontal image
+                cellSize = getRandomNumber(4, 8);
+            }
+            resolve(cellSize);
+        };
+        img.onerror = function () {
+            reject(new Error("Failed to load image on path: " + imgSurce));
+        };
+        img.src = imgSurce;
+    });
+}
 
-// choose random number from 0 to 2 and set cells
-// function chooseCells() {
-//     var cellIndex = Math.floor(Math.random() * 3);
-//     console.log("cellIndex: " + cellIndex);
+function placeImg(imgPosition, cellSize, imageSrc) {
 
-//     var gridCells = document.querySelectorAll('.grid div');
-
-//     if (cellIndex >= 0 && cellIndex < gridCells.length) {
-//         var selectedCell = gridCells[cellIndex];
-//         selectedCell.innerHTML += '.selected/ ';
-//         for (let i = 1; i <= cellIndex; i++) {
-//             var selectedCell = gridCells[cellIndex + i];
-//             selectedCell.innerHTML += '.selected/ ';
-//         }
-//     } else {
-//         console.error('Cell index out of bounds');
-//     }
-// }
-
-function chooseCells() {
-    var cellIndex = Math.floor(Math.random() * 3) + 1;
-    console.log("cellIndex: " + cellIndex);
-
-    var gridCell = document.querySelector(`.grid > div:nth-child(${cellIndex})`);
+    let gridCell = document.querySelector(`.grid > div:nth-child(${imgPosition})`);
     if (gridCell) {
-        gridCell.style.gridColumn = 'span ' + cellIndex;
+        gridCell.style.gridColumn = 'span ' + cellSize;
     }
-    gridCell.innerHTML += '.selected/ ';
+    gridCell.innerHTML += `<article>
+                            <img class="image-in-grid" src="${imageSrc}" alt="this is an image test">
+                            <p>image</p>
+                            </article>`;
 }
 
-function getGridSize() {
-    var defaultScreenWidth = [900, 1200];
-    var gridColumnCount = 48;
-    var screenWidth = window.innerWidth;
+function appendGridCells(grid, numberOfCells) {
+    // Generate an array of the specified length
+    const cellsArray = Array.from({ length: numberOfCells }, (_, i) => i + 1);
+    cellsArray.forEach(cell => {
+        const gridCell = document.createElement('div');
 
-    if (screenWidth <= defaultScreenWidth[0]) {
-        gridColumnCount = gridColumnCount / 4;
-    } else if (screenWidth <= defaultScreenWidth[1]) {
-        gridColumnCount = gridColumnCount / 2;
-    }
-    console.log("gridColumnCount: " + gridColumnCount);
-    return gridColumnCount;
+        grid.appendChild(gridCell);
+    });
 }
 
-function SetPositionForPosts() {
-    var initialPosition = chooseCells();
-    var orderOfPosts = RandomPostsArray();
+function setPosts() {
+    // This is count the order of the images in the grid
+    let imgIndexInGrid = 1;
 
-}
+    let imgPosition = 0;
+    console.log("0: imgPosition: " + imgPosition);
+    let postCount = {{ site.posts.size }};
 
-chooseCells();
+    let orderOfPosts = getRandomPostsArray(postCount);
+    let imageSrc = '/assets/images/project-test/WeedingInGasStation_Greece_2012.webp';
+    const grid = document.querySelector('.grid');
+
+    // Iterate over all images
+    // -1- Define their sizes
+    // -2-  Get their position
+    // -3-  Place them in the grid
+    for (let i = 1; i <= postCount; i++) {
+        // console.log("postCount: " + postCount);
+        defineImageSize(imageSrc)
+            .then(cellSize => {
+                // console.log("Cell size: ", cellSize);
+
+                imgPosition = getRandomNumber(imgPosition + 1, imgPosition + 8);
+                console.log(i + ": imgPosition: " + imgPosition);
+                if (imgPosition <= 8) {
+                    appendGridCells(grid, imgPosition + cellSize);
+                } else {
+                    appendGridCells(grid, cellSize + 1);
+                };
+                
+                placeImg(imgPosition, cellSize, imageSrc);
+                imgPosition++;
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    };
+};
+
+setPosts();
 // Dynamically follow the resizing of screen's width 
-window.addEventListener('resize', function () {
-    getGridSize();
-});
+// window.addEventListener('resize', function () {
+//     getGridSize();
+// });
 
