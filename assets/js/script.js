6a5ecd4c7cfb5210495e333663126478642f9e2d
evitaLocal <evitasima@Kloio>
2024-05-09 00:51:39 +0300

    Filtering options added....working for filtering in all pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,9 @@
 ---
 ---
+let pathname = window.location.pathname
+console.log("pathname: " + pathname);
+if (pathname != '/'){
+    console.log("not index!");
+} else {
+    console.log("index!");
+}
