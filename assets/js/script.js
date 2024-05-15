b1e9011811103b5ce5a6d65873984898b20f9846
evitaLocal <evitasima@Kloio>
2024-05-15 15:02:20 +0200

    Applying grid system for posts in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,29 @@
 ---
 ---
-let pathname = window.location.pathname
-console.log("pathname: " + pathname);
-if (pathname != '/'){
-    console.log("not index!");
-} else {
-    console.log("index!");
+// let pathname = window.location.pathname
+// console.log("pathname: " + pathname);
+// if (pathname != '/'){
+//     console.log("not index!");
+// } else {
+//     console.log("index!");
+// }
+// window.field = pathname;
+
+if (window.location.pathname != "/") {
+    document.addEventListener('DOMContentLoaded', function () {
+        const navLinks = document.querySelectorAll('.nav-link');
+        
+        navLinks.forEach(function (link) {
+            link.addEventListener('click', function (event) {
+                const field = this.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim();
+                // console.log("this is the attribute: " + field);
+                localStorage.field = field;
+                redirectTo("/");
+            });
+        });
+    });
+}
+
+function redirectTo(ref) {
+    window.location.href = ref;
 }
