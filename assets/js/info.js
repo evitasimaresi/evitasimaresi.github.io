387849cbd412241a2c48e4d8f49b58fe073dbbb7
evitaLocal <evitasima@Kloio>
2024-05-21 11:44:03 +0200

    info page working, some work pending

--- assets/js/info.js
+++ assets/js/info.js
@@ -1,9 +1,12 @@
 document.getElementById('reveal-contact').addEventListener('click', function(event) {
     event.preventDefault(); // Prevent the default action of the link
     var contactDetails = document.getElementById('contact-details');
+    var myPhoto = document.getElementsByClassName('my-photo')[0];
     if (contactDetails.classList.contains('visible')) {
         contactDetails.classList.remove('visible');
+        myPhoto.classList.remove('hidden');
     } else {
         contactDetails.classList.add('visible');
+        myPhoto.classList.add('hidden');
     }
 });
