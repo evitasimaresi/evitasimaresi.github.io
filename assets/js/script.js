f5b834a34205dd2acd2097e9402eb92399b23f44
evitaLocal <evitasima@Kloio>
2024-06-03 08:45:18 +0200

    fixing bugs with the navbar, stoping the random appearence of projects, adding new projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,20 @@
 ---
 ---
-// let pathname = window.location.pathname
-// console.log("pathname: " + pathname);
-// if (pathname != '/'){
-//     console.log("not index!");
-// } else {
-//     console.log("index!");
-// }
-// window.field = pathname;
-
-// console.log({{page.url}});
-
-if (window.location.pathname != "/") {
-    document.addEventListener('DOMContentLoaded', function () {
-        const navLinks = document.querySelectorAll('.nav-link');
+// if (window.location.pathname != "/") {
+//     document.addEventListener('DOMContentLoaded', function () {
+//         const navLinks = document.querySelectorAll('.nav-link');
         
-        navLinks.forEach(function (link) {
-            link.addEventListener('click', function (event) {
-                const field = this.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim();
-                // console.log("this is the attribute: " + field);
-                localStorage.field = field;
-                redirectTo("/");
-            });
-        });
-    });
-}
+//         navLinks.forEach(function (link) {
+//             link.addEventListener('click', function (event) {
+//                 const field = this.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim();
+//                 // console.log("this is the attribute: " + field);
+//                 // localStorage.field = field;
+//                 redirectTo("/");
+//             });
+//         });
+//     });
+// }
 
 function redirectTo(ref) {
     window.location.href = ref;
 }
-
